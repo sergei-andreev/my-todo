079f9357ce1d8e179d31ea1f8866cfde47a967cc
sergei-andreev <s.andreev.it@gmail.com>
2019-11-06 23:51:58 +0500

    ready func

--- src/js/View.js
+++ src/js/View.js
@@ -15,7 +15,7 @@ export default class View {
     }
 
     filterHandlers() {
-        ['#rb-1', '#rb-2', '#rb-3'].forEach(selector => {
+        ['#rb-all', '#rb-no', '#rb-yes'].forEach(selector => {
             document.querySelector(selector).addEventListener('change', () => {
                 this.updateView(this.controller.getTodos());
             });
@@ -41,10 +41,95 @@ export default class View {
         });
     }
 
+    viewPlug() {
+        const p = document.createElement('p')
+        p.textContent = 'Nothing to do! Add a task?'
+        this.todoList.append(p)
+    }
+
+    createLi(todo) {
+        const li = document.createElement('li');
+        li.classList.add('list__item', 'list-item');
+        li.dataset.id = todo.id;
+        todo.complete ? li.classList.add('list-item--complete') : 0;
+
+        return li;
+    }
+
+    createLabel() {
+        const label = document.createElement('label');
+        label.classList.add('list-item__label');
+
+        return label;
+    }
+
+    createCheckbox(li) {
+        const checkbox = document.createElement('input');
+        checkbox.classList.add('list-item__checkbox');
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener('click', () => {
+            li.classList.toggle('list-item--complete');
+            this.controller.toggleTodo(li.dataset.id);
+        });
+
+        return checkbox;
+    }
+
+    createMark() {
+        const mark = document.createElement('span');
+        mark.classList.add('list-item__mark');
+
+        return mark;
+
+    }
+
+    createSpan(todo) {
+        const span = document.createElement('span');
+        span.classList.add('list-item__text');
+        span.innerText = todo.text;
+
+        return span;
+    }
+
+    createTodoElement(todo) {
+        const li = this.createLi(todo);
+        const label = this.createLabel();
+        const checkbox = this.createCheckbox(li);
+        const mark = this.createMark();
+        const span = this.createSpan(todo);
+
+        const deleteTodo = document.createElement('button');
+        deleteTodo.classList.add('list-item__btn', 'list-item__btn--delete');
+        deleteTodo.innerHTML = 'd';
+
+        deleteTodo.addEventListener('click', () => {
+            this.controller.deleteTodo(li.dataset.id);
+        });
+
+        const editTodo = document.createElement('button');
+        editTodo.classList.add('list-item__btn', 'list-item__btn--edit');
+        editTodo.innerHTML = 'e';
+
+        editTodo.addEventListener('click', () => {
+            let currentTodoText = span.innerText;
+            let newTodoText = prompt('You edit todo', currentTodoText);
+            newTodoText == null ? newTodoText = currentTodoText : this.controller.editTodo(li.dataset.id, newTodoText);
+        });
+
+        label.appendChild(checkbox);
+        label.appendChild(mark);
+        label.appendChild(span);
+        li.appendChild(label);
+        li.append(deleteTodo);
+        li.append(editTodo);
+
+        return li;
+    }
+
     updateView(todos) {
         this.todoList.innerHTML = '';
 
-        if (document.querySelector('#rb-2').checked) {
+        if (document.querySelector('#rb-no').checked) {
             todos = todos.filter((todo) => {
                 if (todo.complete === false) {
                     return todo;
@@ -52,7 +137,7 @@ export default class View {
             })
         }
 
-        if (document.querySelector('#rb-3').checked) {
+        if (document.querySelector('#rb-yes').checked) {
             todos = todos.filter((todo) => {
                 if (todo.complete === true) {
                     return todo;
@@ -61,60 +146,10 @@ export default class View {
         }
 
         if (todos.length === 0) {
-            const p = document.createElement('p')
-            p.textContent = 'Nothing to do! Add a task?'
-            this.todoList.append(p)
+            this.viewPlug();
         } else {
             todos.forEach(todo => {
-                const li = document.createElement('li');
-                li.classList.add('list__item', 'list-item');
-                li.dataset.id = todo.id;
-                todo.complete ? li.classList.add('list-item--complete') : 0;
-
-                const label = document.createElement('label');
-                label.classList.add('list-item__label');
-
-                const checkbox = document.createElement('input');
-                checkbox.classList.add('list-item__checkbox');
-                checkbox.type = 'checkbox';
-                checkbox.addEventListener('click', () => {
-                    li.classList.toggle('list-item--complete');
-                    this.controller.toggleTodo(li.dataset.id);
-                });
-
-                const mark = document.createElement('span');
-                mark.classList.add('list-item__mark');
-
-                const span = document.createElement('span');
-                span.classList.add('list-item__text');
-                span.innerText = todo.text;
-
-                const deleteTodo = document.createElement('button');
-                deleteTodo.classList.add('list-item__btn', 'list-item__btn--delete');
-                deleteTodo.innerHTML = 'd';
-
-                deleteTodo.addEventListener('click', () => {
-                    this.controller.deleteTodo(li.dataset.id);
-                });
-
-                const editTodo = document.createElement('button');
-                editTodo.classList.add('list-item__btn', 'list-item__btn--edit');
-                editTodo.innerHTML = 'e';
-
-                editTodo.addEventListener('click', () => {
-                    let currentTodoText = span.innerText;
-                    let newTodoText = prompt('You edit todo', currentTodoText);
-                    newTodoText == null ? newTodoText = currentTodoText : this.controller.editTodo(li.dataset.id, newTodoText);
-                    
-                });
-
-                label.appendChild(checkbox);
-                label.appendChild(mark);
-                label.appendChild(span);
-                li.appendChild(label);
-                li.append(deleteTodo);
-                li.append(editTodo);
-
+                const li = this.createTodoElement(todo);
                 this.todoList.appendChild(li);
             });
         }
